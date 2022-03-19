87f40f0735eaeb028433f440f926b4b811a67023
Ju Ju <selenga13@mail.ru>
2022-03-19 21:43:13 +0300

    home work task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,7 +39,7 @@ function sendRequest(name, phone, address, goods, sum) {
     for (let i = 0; i < goods.length; i ++) {
         data.goods.push( {title: goods[i].title, count: goods[i].count});
     }
-
+    
     data.order.address = "ул. " + address.street + ", дом " + address.house 
     + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
     data.order.sum = sum;
